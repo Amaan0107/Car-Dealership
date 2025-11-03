3e74ecbe10f5edc853808d7b0e0aeae037e37ca6
Amaan0107 <Adilawar2007@gmail.com>
2025-11-02 18:54:21 -0800

    working more on 5w since i was fixing 4w last workday

--- src/main/java/org/example/LeaseContract.java
+++ src/main/java/org/example/LeaseContract.java
@@ -3,17 +3,25 @@ package org.example;
 public class LeaseContract extends Contract{
 
     public LeaseContract(String dateOfContract, String customerName,
-                         String customerEmail, String vehicleSold) {
+                         String customerEmail, Vehicle vehicleSold) {
         super(dateOfContract, customerName, customerEmail, vehicleSold);
     }
 
     @Override
     public double calculateMonthlyPayment() {
+    double total = getTotalPrice();
+    double rate = 0.04;
+    int months = 36;
+    return (total * (rate / 12)) / (1 - Math.pow(1 + (rate / 12), -months));
+
 
     }
 
     @Override
     public double calculateTotalPayment() {
-
+        double price = getVehicleSold().getPrice();
+        double expectedEndPayment = price * 0.5;
+        double leaseFee = price * 0.07;
+        return price + expectedEndPayment + leaseFee;
     }
 }
